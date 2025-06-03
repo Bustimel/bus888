a45eee872d3bb862ecbb66e1d3fb6f6149b2e074
Bustimel <bustimelll@gmail.com>
2025-06-03 10:07:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from src.responses import greet_user, unknown_request, confirm_direction
 from src.kb import yes_no_kb
 from src.states import BookingForm
 
-API_TOKEN = "ТВОЙ_ТОКЕН"  # 🔒 ОБОВ’ЯЗКОВО винеси в secrets!
+API_TOKEN = 8011978121:AAEKgT0bCuM5bkb4pm8ddUaWQMKEJGyjFYs
 
 ADMIN_ID = 6132154171
 
