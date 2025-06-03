ae0ee18f2a64ac40ae7160ce298a83fee2376916
Bustimel <bustimelll@gmail.com>
2025-06-03 10:24:00 +0300

    Update states.py

--- src/states.py
+++ src/states.py
@@ -1,7 +1,6 @@
-from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.state import State, StatesGroup
 
 class BookingForm(StatesGroup):
     waiting_for_name = State()
     waiting_for_phone = State()
     waiting_for_date = State()
-    confirming = State()
